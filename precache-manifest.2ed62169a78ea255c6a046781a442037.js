ac46541d3f67f6bf67d3785ab55c8e6e554a5847
Art Mieussens <artmieu@gmail.com>
2019-02-04 05:18:08 +0000

    Updates

---
+++ precache-manifest.2ed62169a78ea255c6a046781a442037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f6a15b3003ed2188cc1",
+    "url": "/robofriends/static/css/main.08f9a11a.chunk.css"
+  },
+  {
+    "revision": "7f6a15b3003ed2188cc1",
+    "url": "/robofriends/static/js/main.7f6a15b3.chunk.js"
+  },
+  {
+    "revision": "eaf5acc10cd8f9c793e5",
+    "url": "/robofriends/static/css/1.a7238842.chunk.css"
+  },
+  {
+    "revision": "eaf5acc10cd8f9c793e5",
+    "url": "/robofriends/static/js/1.eaf5acc1.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1ecbc894f0d8b3f22845d1dfbf636711",
+    "url": "/robofriends/index.html"
+  }
+];
